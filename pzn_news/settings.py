078bc05f2554030ecd6a6757146a71ed81a3d7ef
AliRana1120 <muhammadalirana40702@gmail.com>
2026-08-09 12:35:32 +0500

    Redis added

--- pzn_news/settings.py
+++ pzn_news/settings.py
@@ -152,6 +152,8 @@ APPEND_SLASH = True
 
 
 # ── Celery ────────────────────────────────────────────────────────────────────
+REDIS_URL = config("REDIS_URL")
+
 CELERY_BROKER_URL = config("REDIS_URL", default="redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = config("REDIS_URL", default="redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
